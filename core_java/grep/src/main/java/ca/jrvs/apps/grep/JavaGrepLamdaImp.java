c4dddf0212561d757a058113d54df78a3b8c748e
Priyadarshana Sarma <bharadwaj.priya14@gmail.com>
2022-07-12 15:20:01 -0400

    Updated Grep App with Lamda stream implementation

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLamdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLamdaImp.java
@@ -1,15 +1,24 @@
 package ca.jrvs.apps.grep;
 
-public class JavaGrepLamdaImp extends JavaGrepImp{
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaGrepLamdaImp extends JavaGrepImp {
+
+  final static Logger logger = LoggerFactory.getLogger(JavaGrepLamdaImp.class);
 
   public static void main(String[] args) {
-    //Check for valid number of arguments
+    JavaGrepLamdaImp javaGrepLamdaImp = new JavaGrepLamdaImp();
+
     if (args.length != 3) {
       throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
     }
-    JavaGrepLamdaImp javaGrepLamdaImp = new JavaGrepLamdaImp();
-
-    //setting the regex, rootpath and outFile instance variables
     javaGrepLamdaImp.setRegex(args[0]);
     javaGrepLamdaImp.setRootPath(args[1]);
     javaGrepLamdaImp.setOutFile(args[2]);
@@ -18,6 +27,37 @@ public class JavaGrepLamdaImp extends JavaGrepImp{
       javaGrepLamdaImp.process();
     } catch (Exception ex) {
       logger.error("Error: Unable to process", ex);
+    }
+  }
+
+  @Override
+  public void process() throws IOException {
+    Pattern pattern = Pattern.compile(getRegex());
+    try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(getOutFile()))) {
+
+      //returns a stream of file paths
+      Files.walk(Paths.get(getRootPath()))
+
+          //check if the file is a normal file
+          .filter(Files::isRegularFile)
+          .flatMap(p -> {
+            try {
+              return Files.lines(p);
+            } catch (IOException e) {
+              logger.error(e.getMessage());
+            }
+            return Stream.empty();
+          }).filter(line -> pattern.matcher(line).matches())
+          .forEach(line -> {
+            try {
+              bufferedWriter.write(line);
+              bufferedWriter.newLine();
+            } catch (IOException e) {
+              logger.error(e.getMessage());
+            }
+          });
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
